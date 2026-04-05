eb89aeb710f97ad4de91923b18e757c90dccab88
Sauurravv <cactussaurav@gmail.com>
2026-04-05 21:45:57 +0545

    Fix initial case

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import "./styles/global.css";
 
-import Navbar from "./components/Navbar";
-import Home from "./pages/home";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home";
 import Services from "./pages/Services";
 import Join from "./pages/Join";
 import About from "./pages/About";
